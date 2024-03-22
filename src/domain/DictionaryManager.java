cb81b1acf27e2581e09a7d8312a4c2e6eb6fb204
danielmoreno <dmoreno@indeed.com>
2024-03-22 13:54:46 -0500

    Find word method removed bug concerning spaces. Function can now find words

--- src/domain/DictionaryManager.java
+++ src/domain/DictionaryManager.java
@@ -5,20 +5,16 @@ import interfaces.WordReader;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class DictionaryManager {
 
-    public DictionaryManager() throws IOException {
-    }
 
-    // Checks if the dictionary exists
-    // If not, it will create one
-    public void dictionaryExists() {
-        File dictionaryFile = new File("./src/lib/dictionary.txt");
+    // ======== METHOD TO STORE CONTENT OF DICTIONARY IN AN ARRAY =============
+    public List<Word> loadDictionary() throws IOException { // returns list of word objects
 
+        // First checks if the dictionary exists
+        File dictionaryFile = new File("./lib/dictionary.txt");
         if (!dictionaryFile.exists()) {
             try {
                 if (dictionaryFile.createNewFile()) {
@@ -30,204 +26,187 @@ public class DictionaryManager {
                 throw new RuntimeException("Error: " + e.getMessage());
             }
         }
-    }
 
-    // ======== METHOD TO STORE CONTENT OF DICTIONARY IN AN ARRAY =============
-    public List<String[]> loadDictionary() throws IOException {
         // Load dictionary from the file
         WordReader wordReader = new domain.WordReader();
-        String fullDictionaryString = wordReader.readEntries("./lib/dictionary.txt"); // took what the method returned and assigned to a String variable
+        String fullDictionary = wordReader.readEntries("./lib/dictionary.txt"); // took what the method returned and assigned to a String variable
 
         // splitting the string and turning it into an array
         // each line is an element in the array
-        String[] dictionaryEntries = fullDictionaryString.split("\\n");
+        String[] dictionaryEntries = fullDictionary.split("\\n");
 
-        // Create an ArrayList to hold the arrays
-        List<String[]> arrayList = new ArrayList<>();
+        List<Word> words = Arrays.stream(dictionaryEntries)
+                .map(entry -> entry.split(" \\| "))
+                .map(splitEntry -> new Word(splitEntry[0], splitEntry[1], splitEntry[2], splitEntry[3]))
+                .toList();
 
-        // Turn each line into a list that is split at the '|' symbol
-        for (String entry : dictionaryEntries) {
-            // split at the ' | '
-            String[] wordInfo = entry.split(" \\| ");
-
-            // add every line into an arrayList
-            arrayList.add(wordInfo);
-        }
-        System.out.println(arrayList);
-        return arrayList;
+        return words;
     }
 
     public void findWord() throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the word(s) you would like to find. If more than one, please separate them by commas.");
         String wordInput = scanner.nextLine().trim(); // trim whatever input so it has no trailing spaces
-        String[] wordSearch; // creating an array to hold the words in the search
-
-        if (wordInput.contains(",")) {
-            // multiple words searched separated by commas
-            wordSearch = wordInput.split(","); // turn search into a list of Strings
-            for (String word : wordSearch) {
-                if (word.trim().contains(" ")) { // trimming words then checking if it contains spaces within it
-                    System.out.println("Error: please ensure there are no spaces in your words");
-                    return;
-                }
-            }
-        } else if (wordInput.contains(" ")) {
-            // multiple words searched but they have spaces in them
-            System.out.println("Error: please separate the words by commas or ensure there are no spaces in your words.");
-            return;
-        } else {
-            // only one word searched
-            wordSearch = new String[1]; // creates a string with one element
-            wordSearch[0] = wordInput.trim(); // fills element with the one word that was searched
-        }
+        String[] wordSearch = wordInput.split(","); // turn search into a list of Strings
 
         // Load dictionary entries
-        List<String[]> dictionaryEntries = loadDictionary();
+        List<Word> dictionaryEntries = loadDictionary();
 
         // Search every dictionary entry for the words  in the wordSearch
-        for (String[] entry : dictionaryEntries) {
-            String dictionaryWord = entry[0]; // finds the first element of the entry, which is the word
-            for (String word : wordSearch) {
-                if (dictionaryWord.toLowerCase().equalsIgnoreCase(word.toLowerCase())) {
-                    System.out.println(String.join(" | ", entry));
+        List<Word> matchingEntries = new ArrayList<>();
+
+        for (Word entry : dictionaryEntries) {
+            String word = entry.getWord(); // finds the word
+            for (String searchedWord : wordSearch) {
+                if (searchedWord.trim().equalsIgnoreCase(word)) {
+                    matchingEntries.add(entry);
                 }
             }
         }
-    }
 
-    public void findDefinition() throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter word(s) you would like to find in definitions. If more than one, please separate them by commas.");
-        String definitionInput = scanner.nextLine().trim(); // trim whatever input so it has no trailing spaces
-        String[] definitionSearch; // creating an array to hold the words in the search
-
-        if (definitionInput.contains(",")) {
-            // multiple words searched separated by commas
-            definitionSearch = definitionInput.split(","); // turn search into a list of Strings
-            for (String definitionWord : definitionSearch) {
-                if (definitionWord.trim().contains(" ")) { // trimming words then checking if it contains spaces within it
-                    System.out.println("Error: please ensure there are no spaces in your words");
-                    return;
-                }
-            }
-        } else if (definitionInput.contains(" ")) {
-            // multiple words searched but they have spaces in them
-            System.out.println("Error: please separate the words by commas or ensure there are no spaces in your words.");
-            return;
+        // Print matching entries
+        if (matchingEntries.isEmpty()) {
+            System.out.println("No matching words found.");
         } else {
-            // only one word searched
-            definitionSearch = new String[1]; // creates a string with one element
-            definitionSearch[0] = definitionInput.trim(); // fills element with the one word that was searched
-        }
-
-        // Load dictionary entries
-        List<String[]> dictionaryEntries = loadDictionary();
-
-        // Search every dictionary entry for the words in the definitionSearch
-        for (String[] entry : dictionaryEntries) {
-            String wordDefinition = entry[1]; // finds the first second of the entry, which is the definition
-            for (String word : definitionSearch) {
-                if (wordDefinition.toLowerCase().contains(word.toLowerCase())) {
-                    System.out.println(String.join(" | ", entry));
-                }
+            System.out.println("Matching entries:");
+            for (Word entry : matchingEntries) {
+                System.out.println(entry);
             }
         }
     }
 
-    public void findStartsWith() throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Find words that start with: ");
-        String prefixSearch = scanner.nextLine().trim(); // trim whatever input so it has no trailing spaces
-
-        // Load dictionary entries
-        List<String[]> dictionaryEntries = loadDictionary();
-
-        // Search for words starting with the prefix
-        List<String[]> entriesWithPrefix = new ArrayList<>(); // creating array list that has words starting with the prefix
-
-        for (String[] entry : dictionaryEntries) {
-            String word = entry[0]; // the word is at index 0 of every list
-            if (word.toLowerCase().startsWith(prefixSearch.toLowerCase())) {
-                entriesWithPrefix.add(entry); // add the matching entries to the wordsWithPrefix arrayList
-            }
-        }
-
-        // Printing matching entries
-        if (entriesWithPrefix.isEmpty()) { // checks if the arrayList is empty
-            System.out.println("There are no words starting with '" + prefixSearch + "'.");
-        } else {
-            for (String[] entry : entriesWithPrefix) {
-                System.out.println(String.join(" | ", entry));
-            }
-        }
-    }
-
-    public void findEndsWith() throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Find words that end with: ");
-        String suffixSearch = scanner.nextLine().trim(); // trim whatever input so it has no trailing spaces
-
-        // Load dictionary entries
-        List<String[]> dictionaryEntries = loadDictionary();
-
-        // Search for words starting with the prefix
-        List<String[]> entriesWithSuffix = new ArrayList<>(); // creating array list that has words starting with the prefix
-
-        for (String[] entry : dictionaryEntries) {
-            String word = entry[0]; // the word is at index 0 of every list
-            if (word.toLowerCase().endsWith(suffixSearch.toLowerCase())) {
-                entriesWithSuffix.add(entry); // add the matching entries to the wordsWithPrefix arrayList
-            }
-        }
-
-        // Printing matching entries
-        if (entriesWithSuffix.isEmpty()) { // checks if the arrayList is empty
-            System.out.println("There are no words ending with '" + suffixSearch + "'.");
-        } else {
-            for (String[] entry : entriesWithSuffix) {
-                System.out.println(String.join(" | ", entry));
-            }
-        }
-    }
-
-    public void findSubstring() throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the substring(s) you would like to find in a word. If more than one, please separate them by commas.");
-        String substringInput = scanner.nextLine().trim(); // trim whatever input so it has no trailing spaces
-        String[] substringSearch; // creating an array to hold the words in the search
-
-        if (substringInput.contains(",")) {
-            // multiple words searched separated by commas
-            substringSearch = substringInput.split(","); // turn search into a list of Strings
-            for (String word : substringSearch) {
-                if (word.trim().contains(" ")) { // trimming words then checking if it contains spaces within it
-                    System.out.println("Error: please ensure there are no spaces in your substrings");
-                    return;
-                }
-            }
-        } else if (substringInput.contains(" ")) {
-            // multiple words searched but they have spaces in them
-            System.out.println("Error: please separate the substrings by commas or ensure there are no spaces in your substrings.");
-            return;
-        } else {
-            // only one word searched
-            substringSearch = new String[1]; // creates a string with one element
-            substringSearch[0] = substringInput.trim(); // fills element with the one word that was searched
-        }
-
-        // Load dictionary entries
-        List<String[]> dictionaryEntries = loadDictionary();
-
-        // Search every dictionary entry for the words  in the wordSearch
-        for (String[] entry : dictionaryEntries) {
-            String dictionaryWord = entry[0]; // finds the first element of the entry, which is the word
-            for (String substring : substringSearch) {
-                if (dictionaryWord.toLowerCase().contains(substring.toLowerCase())) {
-                    System.out.println(String.join(" | ", entry));
-                }
-            }
-        }
-    }
+//    public void findDefinition() throws IOException {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter word(s) you would like to find in definitions. If more than one, please separate them by commas.");
+//        String definitionInput = scanner.nextLine().trim(); // trim whatever input so it has no trailing spaces
+//        String[] definitionSearch; // creating an array to hold the words in the search
+//
+//        if (definitionInput.contains(",")) {
+//            // multiple words searched separated by commas
+//            definitionSearch = definitionInput.split(","); // turn search into a list of Strings
+//            for (String definitionWord : definitionSearch) {
+//                if (definitionWord.trim().contains(" ")) { // trimming words then checking if it contains spaces within it
+//                    System.out.println("Error: please ensure there are no spaces in your words");
+//                    return;
+//                }
+//            }
+//        } else if (definitionInput.contains(" ")) {
+//            // multiple words searched but they have spaces in them
+//            System.out.println("Error: please separate the words by commas or ensure there are no spaces in your words.");
+//            return;
+//        } else {
+//            // only one word searched
+//            definitionSearch = new String[1]; // creates a string with one element
+//            definitionSearch[0] = definitionInput.trim(); // fills element with the one word that was searched
+//        }
+//
+//        // Load dictionary entries
+//        List<String[]> dictionaryEntries = loadDictionary();
+//
+//        // Search every dictionary entry for the words in the definitionSearch
+//        for (String[] entry : dictionaryEntries) {
+//            String wordDefinition = entry[1]; // finds the first second of the entry, which is the definition
+//            for (String word : definitionSearch) {
+//                if (wordDefinition.toLowerCase().contains(word.toLowerCase())) {
+//                    System.out.println(String.join(" | ", entry));
+//                }
+//            }
+//        }
+//    }
+//
+//    public void findStartsWith() throws IOException {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Find words that start with: ");
+//        String prefixSearch = scanner.nextLine().trim(); // trim whatever input so it has no trailing spaces
+//
+//        // Load dictionary entries
+//        List<String[]> dictionaryEntries = loadDictionary();
+//
+//        // Search for words starting with the prefix
+//        List<String[]> entriesWithPrefix = new ArrayList<>(); // creating array list that has words starting with the prefix
+//
+//        for (String[] entry : dictionaryEntries) {
+//            String word = entry[0]; // the word is at index 0 of every list
+//            if (word.toLowerCase().startsWith(prefixSearch.toLowerCase())) {
+//                entriesWithPrefix.add(entry); // add the matching entries to the wordsWithPrefix arrayList
+//            }
+//        }
+//
+//        // Printing matching entries
+//        if (entriesWithPrefix.isEmpty()) { // checks if the arrayList is empty
+//            System.out.println("There are no words starting with '" + prefixSearch + "'.");
+//        } else {
+//            for (String[] entry : entriesWithPrefix) {
+//                System.out.println(String.join(" | ", entry));
+//            }
+//        }
+//    }
+//
+//    public void findEndsWith() throws IOException {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Find words that end with: ");
+//        String suffixSearch = scanner.nextLine().trim(); // trim whatever input so it has no trailing spaces
+//
+//        // Load dictionary entries
+//        List<String[]> dictionaryEntries = loadDictionary();
+//
+//        // Search for words starting with the prefix
+//        List<String[]> entriesWithSuffix = new ArrayList<>(); // creating array list that has words starting with the prefix
+//
+//        for (String[] entry : dictionaryEntries) {
+//            String word = entry[0]; // the word is at index 0 of every list
+//            if (word.toLowerCase().endsWith(suffixSearch.toLowerCase())) {
+//                entriesWithSuffix.add(entry); // add the matching entries to the wordsWithPrefix arrayList
+//            }
+//        }
+//
+//        // Printing matching entries
+//        if (entriesWithSuffix.isEmpty()) { // checks if the arrayList is empty
+//            System.out.println("There are no words ending with '" + suffixSearch + "'.");
+//        } else {
+//            for (String[] entry : entriesWithSuffix) {
+//                System.out.println(String.join(" | ", entry));
+//            }
+//        }
+//    }
+//
+//    public void findSubstring() throws IOException {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter the substring(s) you would like to find in a word. If more than one, please separate them by commas.");
+//        String substringInput = scanner.nextLine().trim(); // trim whatever input so it has no trailing spaces
+//        String[] substringSearch; // creating an array to hold the words in the search
+//
+//        if (substringInput.contains(",")) {
+//            // multiple words searched separated by commas
+//            substringSearch = substringInput.split(","); // turn search into a list of Strings
+//            for (String word : substringSearch) {
+//                if (word.trim().contains(" ")) { // trimming words then checking if it contains spaces within it
+//                    System.out.println("Error: please ensure there are no spaces in your substrings");
+//                    return;
+//                }
+//            }
+//        } else if (substringInput.contains(" ")) {
+//            // multiple words searched but they have spaces in them
+//            System.out.println("Error: please separate the substrings by commas or ensure there are no spaces in your substrings.");
+//            return;
+//        } else {
+//            // only one word searched
+//            substringSearch = new String[1]; // creates a string with one element
+//            substringSearch[0] = substringInput.trim(); // fills element with the one word that was searched
+//        }
+//
+//        // Load dictionary entries
+//        List<String[]> dictionaryEntries = loadDictionary();
+//
+//        // Search every dictionary entry for the words  in the wordSearch
+//        for (String[] entry : dictionaryEntries) {
+//            String dictionaryWord = entry[0]; // finds the first element of the entry, which is the word
+//            for (String substring : substringSearch) {
+//                if (dictionaryWord.toLowerCase().contains(substring.toLowerCase())) {
+//                    System.out.println(String.join(" | ", entry));
+//                }
+//            }
+//        }
+//    }
 
 }
