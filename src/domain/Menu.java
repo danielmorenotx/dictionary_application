c6cb289ea01bcfb3219299f7829b2b747ee10541
danielmoreno <dmoreno@indeed.com>
2024-03-22 11:59:35 -0500

    using Switch for menu, now prompts to see menu or exit at the beginning and after each method

--- src/domain/Menu.java
+++ src/domain/Menu.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
 
 public class Menu {
 
-    public void menuHandler() throws IOException {
-        int choiceInput;
+    public void enterToContinue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press 'Enter' to see the main menu. Press 'Escape' to exit the application.");
+        String continueChoice = scanner.nextLine();
 
+        if (continueChoice.equalsIgnoreCase("Escape")) {
+            System.out.println("Exiting application...");
+            System.exit(0);
+        } else {
+            displayMenu();
+        }
+    }
+
+    public void displayMenu() {
         System.out.println("****************************************************\n" +
                 "Welcome. Please pick a number of one of the following options:\n" +
                 "\n" +
@@ -22,53 +33,59 @@ public class Menu {
                 "9. Creator\n" +
                 "10. Exit\n" +
                 "****************************************************");
+    }
+
+    public void menuHandler() throws IOException {
         Scanner scanner = new Scanner(System.in);
-
-        try {
-            choiceInput = scanner.nextInt();
-            scanner.nextLine();
-        } catch (Exception e) {
-            System.out.println("Invalid input. Please ensure your input is a number");
-            return;
-        }
-
         DictionaryManager wordFinder = new DictionaryManager();
-        if (choiceInput == 1) {
-            // find word(s)
-            wordFinder.findWord();
-        } else if (choiceInput == 2) {
-            // find words by definition
-            wordFinder.findDefinition();
-        } else if (choiceInput == 3) {
-            // Find all words that start with
 
-        } else if (choiceInput == 4) {
-            // Find all words that end with
+        while (true) {
+            int choiceInput;
+            try {
+                choiceInput = scanner.nextInt();
+                scanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please ensure your input is a number");
+                return;
+            }
 
-        } else if (choiceInput == 5) {
-            // Find all words containing
-            wordFinder.findSubstring();
-        } else if (choiceInput == 6) {
-            // Add a word
-            WordWriter wordWriter = new WordWriter();
-            wordWriter.writeEntries();
-        } else if (choiceInput == 7) {
-            // Delete a word
-
-        } else if (choiceInput == 8) {
-            // History
-
-        } else if (choiceInput == 9) {
-            // Creator
-            System.out.println("This dictionary app was created by Daniel Moreno.");
-
-        } else if (choiceInput == 10) {
-            // Exit
-            System.out.println("Exiting application...");
-            System.exit(0);
-
-        } else {
-
+            switch (choiceInput) {
+                case 1:
+                    wordFinder.findWord();
+                    enterToContinue();
+                    break;
+                case 2:
+                    //wordFinder.findDefinition();
+                    break;
+                case 3:
+                    //wordFinder.findStartsWith();
+                    break;
+                case 4:
+                    //wordFinder.findEndsWith();
+                    break;
+                case 5:
+                    //wordFinder.findSubstring();
+                    break;
+                case 6:
+                    WordWriter wordWriter = new WordWriter();
+                    wordWriter.writeEntries();
+                    break;
+                case 7:
+                    // Delete a word
+                    break;
+                case 8:
+                    // History
+                    break;
+                case 9:
+                    System.out.println("This dictionary app was created by Daniel Moreno.");
+                    break;
+                case 10:
+                    System.out.println("Exiting application...");
+                    return; // exit the method
+                default:
+                    System.out.println("Error: Invalid input. Please try again.");
+                    break;
+            }
         }
     }
 }
