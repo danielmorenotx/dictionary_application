0374d3f1963304f74b77fed50ee467d04dcbf8c3
danielmoreno <dmoreno@indeed.com>
2024-03-26 10:41:30 -0500

    utilities class added methods to validate inputs

--- src/domain/Utilities.java
+++ src/domain/Utilities.java
@@ -1,51 +1,43 @@
 package domain;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Utilities {
 
-    public static String partOfSpeechValidator() {
-        Scanner scanner = new Scanner(System.in);
+    public static boolean isValidWord(String word) {
+        return word.toLowerCase().matches("[a-z]+"); // checks if word is only letters
+    }
 
-        while (true) {
-            System.out.println("1. noun - the name of a person, place, thing, or idea.\n" +
-                    "2. verb - expresses action or being.\n" +
-                    "3. adjective - modifies or describes a noun or pronoun.\n" +
-                    "4. adverb - modifies or describes a verb, an adjective, or another adverb.\n" +
-                    "5. pronoun - a word used in place of a noun.\n" +
-                    "6. preposition - a word placed before a noun or pronoun to form a phrase modifying another word in the sentence.\n" +
-                    "7. conjunction - joins words, phrases, or clauses.\n" +
-                    "8. interjection - a word used to express emotion.\n");
+    public static boolean isValidDefinitionOrExample(String definitionOrExample) {
+        return definitionOrExample.split(" ").length > 2; // checks if the definition has more than two words
+    }
 
-            // Read user input
-            int partOfSpeechInput = scanner.nextInt();
-            scanner.nextLine(); // Consume newline character
+    public static String partOfSpeechValidator(String input) {
 
-            switch (partOfSpeechInput) {
-                case 1:
-                    return "noun";
-                case 2:
-                    return "verb";
-                case 3:
-                    return "adjective";
-                case 4:
-                    return "adverb";
-                case 5:
-                    return "pronoun";
-                case 6:
-                    return "preposition";
-                case 7:
-                    return "conjunction";
-                case 8:
-                    return "interjection";
-                default:
-                    throw new IllegalArgumentException("Invalid Input. Please enter the correct number corresponding to the word's part of speech:\n");
+        switch (input) {
+            case "1":
+                return "noun";
+            case "2":
+                return "verb";
+            case "3":
+                return "adjective";
+            case "4":
+                return "adverb";
+            case "5":
+                return "pronoun";
+            case "6":
+                return "preposition";
+            case "7":
+                return "conjunction";
+            case "8":
+                return "interjection";
+            default:
+                System.out.println("Invalid Input. Please enter the correct number corresponding to the word's part of speech:\n");
+                return "invalid";
 
-            }
         }
     }
 
+    public static boolean newEntryConfirmation(String confirmation) {
+        return confirmation.isEmpty();
+    }
 }
+
