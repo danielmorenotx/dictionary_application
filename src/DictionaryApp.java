fc8990117d661460e456821feac97ee48d5f6c77
danielmoreno <dmoreno@indeed.com>
2024-03-27 11:56:52 -0500

    completed dictionary, with an exception if dictionary can't be accessed

--- src/DictionaryApp.java
+++ src/DictionaryApp.java
@@ -1,5 +1,17 @@
-public class DictionaryApp {
-    public static void main(String[] args) {
+import domain.DictionaryManager;
+import domain.Menu;
 
+import java.io.IOException;
+
+public class DictionaryApp {
+    public static void main(String[] args) throws IOException {
+
+        try {
+            DictionaryManager.loadDictionary(); // Load the dictionary
+            Menu menu = new Menu();
+            menu.displayMenu();
+        } catch (IOException e) {
+            throw new IOException("Error loading dictionary: " + e.getMessage()); // if the dictionary gets an IO exception, program dies
+        }
     }
 }
